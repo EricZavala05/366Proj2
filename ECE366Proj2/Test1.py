bd6990b94831c6c1df5a70d68b8bbfb3c54b97ba
BauerEricR <32853412+BauerEricR@users.noreply.github.com>
2019-02-19 07:45:40 -0600

    Add files via upload

--- ECE366Proj2/Test1.py
+++ ECE366Proj2/Test1.py
@@ -28,6 +28,7 @@ def hextobin(hexitem):
     #print(hexterms)
     c=0
     for term in hexterms:
+    #select only the last 8 characters in each line
         hexterms[c] = term[-8:]
         c+=1  
     k=0
@@ -52,6 +53,8 @@ class mipsMachine:
         self.code = arra
         self.pc = 0
         self.offset = 0x2000
+        #instruction count [total, r, i, j]
+        self.count = [0]*4
       
     def execute(self):
         bottom = len(self.code.tolist())
@@ -68,49 +71,61 @@ class mipsMachine:
                 self.jtype(command)
                 #j-type
             self.pc += 1
+            #print('PC is 0x'+str(format(self.pc*4,'08x')))
             self.reg[0] = 0 #reg0 is always 0
+            self.count[0]+=1
         print('fell off bottom')
+
+        
     def rtype(self,command):
+        self.count[1]+=1
         #extract values from command
         rs = 0x1f & (command >> 21)
         rt = 0x1f & (command >> 16)
         rd = 0x1f & (command >> 11)
-        opcode = 0x3f & command 
+        sh = 0x1f & (command >> 6)
+        func = 0x3f & command 
 ##        print("execute r-type")
 ##        print(format(command,'032b'))
 ##        print(rs)
 ##        print(rt)
 ##        print(rd)
-        if opcode == 0x20:#add
+        if func == 0x00:#sll
+            self.reg[rd] = self.reg[rt] << sh
+        elif func == 0x02:#srl
+            self.reg[rd] = self.reg[rt] >> sh
+        elif func == 0x20:#add
             self.reg[rd]=self.reg[rs]+self.reg[rt]
 	    #todo error handling
-        elif opcode == 0x21:#addu
+        elif func == 0x21:#addu
             self.reg[rd]=self.reg[rs]+self.reg[rt]
-        elif opcode == 0x22:#sub
+        elif func == 0x22:#sub
             self.reg[rd]=self.reg[rs]-self.reg[rt]
             #error handling?
-        elif opcode == 0x23:#subu
+        elif func == 0x23:#subu
             self.reg[rd]=self.reg[rs]-self.reg[rt]
-        elif opcode == 0x24:#and
+        elif func == 0x24:#and
             self.reg[rd]=self.reg[rs]&self.reg[rt]
-        elif opcode == 0x25:#or
+        elif func == 0x25:#or
             self.reg[rd]=self.reg[rs]|self.reg[rt]
-        elif opcode == 0x26:#xor
+        elif func == 0x26:#xor
             self.reg[rd]=self.reg[rs]^self.reg[rt]
-        elif opcode == 0x27:#nor
+        elif func == 0x27:#nor
             self.reg[rd]= ~(self.reg[rs]|self.reg[rt])
-        elif opcode == 0x2a:#slt
+        elif func == 0x2a:#slt
             if self.reg[rs] < self.reg[rt]:
                 self.reg[rd] = 1
             else:
                 self.reg[rd] = 0
-        elif opcode == 0x2b:#sltu
+        elif func == 0x2b:#sltu
             if self.reg[rs] < self.reg[rt]:
                 self.reg[rd] = 1
             else:
                 self.reg[rd] = 0
+        
 
     def itype(self,command):
+        self.count[2]+=1
         #extract values from command
         opcode= 0x3f & (command >> 26)
         rs = 0x1f & (command >> 21)
@@ -121,7 +136,7 @@ class mipsMachine:
 ##        print(format(command,'032b'))
 ##        print('rs: '+str(rs)+' is 0x' +str(format(self.reg[rs],'08x')))
 ##        print('rt: '+str(rt)+' is ' +str(self.reg[rt]))
-##        print(imm)
+##        print('imm is '+str(imm))
         if opcode == 4:#beq
             if self.reg[rs] == self.reg[rt]:
                 self.pc += imm
@@ -132,8 +147,12 @@ class mipsMachine:
             self.reg[rt]= self.reg[rs] + imm
         if opcode ==0x2b:#sw
             self.mem[(self.reg[rs]+imm-self.offset)>>2] = self.reg[rt]
+        if opcode ==0x23:#lw
+             self.reg[rt] = self.mem[(self.reg[rs]+imm-self.offset)>>2]
+        
 
     def jtype(self,command):
+        self.count[3]+=1
         #extract values from command
         addr = 0x3ffffff & (command >> 21)
         opcode = 0x3f & (command >> 26) 
@@ -141,17 +160,22 @@ class mipsMachine:
 ##        print(format(command,'032b'))
         if opcode == 2:
             self.pc = (((self.pc<<2)&0xf0000000)| addr<<2) >> 2
+        
 
     def result(self):
         print('Registers:')
         for thing in self.reg:
-            print('0x'+str(format(thing,'0x')))
+            print(hex(thing & 0xffffffff))
         c=0
-        print('Memory:')
+        print('Memory: Hex  Decimal')
         while c < 0x14:
-            print(str(format(self.offset+(c*4),'0x'))+':  '+str(self.mem[c]))
+            print(str(format(self.offset+(c*4),'0x'))+':  '+hex(self.mem[c]&0xffffffff)+'   '+str(self.mem[c]))
             c+=1
-
+        print('Total instructions run: '+str(self.count[0]))
+        print('R-type: '+str(self.count[1]))
+        print('I-type: '+str(self.count[2]))
+        print('J-type: '+str(self.count[3]))
+        
     
 #end mipsMachine class
 infile = open("in.txt", 'r')
@@ -159,13 +183,12 @@ mipshex = infile.read()
 binary = hextobin(mipshex)
 outfile = open('out.txt','w')
 #to check my work
-##for instruction in binary:
-##    outfile.write(format(instruction, '032b')+'\n')
-##outfile.close()
-##print('doesn\'t crash 0')
+for instruction in binary:
+    outfile.write(format(instruction, '032b')+'\n')
+outfile.close()
 
 order66 = mipsMachine(binary)
-#execute order 66.
+
 order66.execute()
 
 order66.result()
